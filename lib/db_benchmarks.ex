61e36e974910afd1f94b5479cba8a2e0dee2b0e9
claudioStahl <claudioarosseto@gmail.com>
2022-07-15 19:28:10 -0300

    init project

---
+++ lib/db_benchmarks.ex
@@ -0,0 +1,3 @@
+defmodule DbBenchmarks do
+  @moduledoc false
+end
---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule DbBenchmarks.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :db_benchmarks,
+      version: "0.1.0",
+      elixir: "~> 1.13",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger]
+    ]
+  end
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.6"},
+      {:postgrex, ">= 0.0.0"},
+      {:timex, "~> 3.7"},
+      {:benchee, "~> 1.1"}
+    ]
+  end
+end
