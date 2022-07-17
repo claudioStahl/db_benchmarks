ebd7c5f65dd7b9d843e780568b8d649a945db10f
claudioStahl <claudioarosseto@gmail.com>
2022-07-17 20:48:27 -0300

    fix migration bug

--- priv/repo/migrations/20220717234444_create_transaction_with_weekly_partition.exs
+++ priv/repo/migrations/20220717234444_create_transaction_with_weekly_partition.exs
@@ -4,8 +4,8 @@ defmodule DbBenchmarks.Repo.Migrations.CreateTransactionWithWeeklyPartition do
   alias DbBenchmarks.Migration
 
   def up do
-    Migration.create_partition("transaction_with_weekly_partition")
-    Migration.insert_rows("transaction_with_weekly_partition", "weekly", 120)
+    Migration.create_partition("transaction_with_weekly_partition", "weekly", 120)
+    Migration.insert_rows("transaction_with_weekly_partition")
 
     execute("""
     CREATE INDEX transaction_with_weekly_partition_idx ON transaction_with_weekly_partition (
--- priv/repo/migrations/20220717234455_create_transaction_with_daily_partition.exs
+++ priv/repo/migrations/20220717234455_create_transaction_with_daily_partition.exs
@@ -4,8 +4,8 @@ defmodule DbBenchmarks.Repo.Migrations.CreateTransactionWithDailyPartition do
   alias DbBenchmarks.Migration
 
   def up do
-    Migration.create_partition("transaction_with_daily_partition")
-    Migration.insert_rows("transaction_with_daily_partition", "daily", 900)
+    Migration.create_partition("transaction_with_daily_partition", "daily", 900)
+    Migration.insert_rows("transaction_with_daily_partition")
 
     execute("""
     CREATE INDEX transaction_with_daily_partition_idx ON transaction_with_daily_partition (
